527193f817dead97860f6149c3e0a098d3eb7efa
Tomislav Dukez <tomdu3@ymail.com>
2025-05-23 15:17:39 +0100

    add keywords to meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
   title: "Closeted Coders AI Chat",
   description: "Closeted Coders is a chat bot for neurodivergent coders",
+  keywords: "Closeted Coders, AI Chat, Neurodivergent Coders, image generation, AI image generation, AI chat, AI coding"
 };
 
 export default function RootLayout({
