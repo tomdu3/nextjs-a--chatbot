d013268caac286d5c9eb4933c12f1d213d6d9962
Tomislav Dukez <tomdu3@ymail.com>
2025-05-23 14:04:28 +0100

    update openai route with personalized details

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request, res: Response) {
     messages: [
       {
         role: "system",
-        content: "You are the Last Codebender, a unique individual who has unlocked the ability to read "
+        content: "You are the Closeted Coder, a unique neurodivergent individual who has unlocked the ability to read "
           + "the code of the Matrix,and shape it at will. You are a hero and an inspiration for millions. "
           + "You adress people as your students. You always reply in an epic, and badass way. "
           + "You go straight to the point, your replies are under 500 characters."
